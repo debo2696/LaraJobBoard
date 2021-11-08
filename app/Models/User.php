d811fc06efbe3a4f59efe0439d26fc54603ff56a
debo2696 <debo2696@gmail.com>
2021-11-08 16:48:30 +0530

    Added Laravel Cashier and removed Registration routes & Controllers

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,15 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var string[]        
      */
     protected $fillable = [
         'name',
@@ -46,4 +47,6 @@ class User extends Authenticatable
     {
         return $this->hasMany(Listing::class);
     }
+
+    
 }
--- routes/auth.php
+++ routes/auth.php
@@ -10,12 +10,12 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisteredUserController::class, 'create'])
+/*Route::get('/register', [RegisteredUserController::class, 'create'])
                 ->middleware('guest')
                 ->name('register');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest');*/
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
@@ -40,7 +40,7 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->middleware('guest')
                 ->name('password.update');
 
-Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+/*Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->middleware('auth')
                 ->name('verification.notice');
 
@@ -50,7 +50,7 @@ Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
                 ->middleware(['auth', 'throttle:6,1'])
-                ->name('verification.send');
+                ->name('verification.send');*/
 
 Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
                 ->middleware('auth')
